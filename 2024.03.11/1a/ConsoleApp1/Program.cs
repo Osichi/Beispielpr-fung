69ba0f7c2b47f96f7377168778f5fc9cc3466ca1
Norbert Rácz <norbert.russov@gmail.com>
2024-03-11 08:45:56 +0100

    c# ist einfach

---
+++ 2024.03.11/1a/ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//2
+            List<Books> books = new List<Books>();
+            string[] strings = File.ReadAllLines("books.txt");
+            //3
+            foreach (string s in strings)
+            {
+                string[] values = s.Split(',');
+                Books books1 = new Books(values[0], values[1], values[2], values[3], values[4]);
+                books.Add(books1);
+            }
+            //4
+            Console.WriteLine("4. feladat");
+            foreach(Books book in books)
+            {
+                Console.WriteLine($"{book.sorszam} {book.mufaj} {book.cim} {book.ar} {book.darab}");
+            }
+
+            //5
+            Console.WriteLine("5. feladat");
+            int db = 0;
+            foreach(Books book in books)
+            {
+                db += book.darab;
+            }
+            Console.WriteLine($"Az össz darabszám: {db} db");
+
+            //6
+            Console.WriteLine("6. feladat");
+            foreach (var book in books)
+            {
+                if (book.mufaj == "Regény")
+                {
+                    Console.WriteLine($"A regény kategóriában lévő könyv címe és ára: {book.mufaj}, {book.cim}, {book.ar}");
+                }
+            }
+
+            //7
+            Console.WriteLine("7. feladat");
+
+            Dictionary<string, int> keyValuePairs = new Dictionary<string, int>();
+            foreach (var book in books)
+            {
+                if (keyValuePairs.ContainsKey(book.mufaj))
+                {
+                keyValuePairs[book.mufaj]++;
+                }
+                else
+                {
+                    keyValuePairs[book.mufaj] = 1;
+                }
+            }
+            Console.WriteLine("Kategóriák és termékek száma: ");
+            foreach (var item in keyValuePairs)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} termék");
+            }
+
+            //8
+            Console.WriteLine("8. feladat");
+
+            List<Books> legolcsobbak = new List<Books>();
+            Books legolcsobb = books[0];
+            legolcsobbak.Add(legolcsobb);
+            foreach (var book in books)
+            {
+                if (book.ar < legolcsobb.ar)
+                {
+                    legolcsobb = book;
+                    legolcsobbak.Clear();
+                    legolcsobbak.Add(legolcsobb);
+                }
+                else if (book.ar == legolcsobb.ar)
+                {
+                    legolcsobbak.Add(book);
+                }
+            }
+
+            Console.WriteLine("\nLegolcsóbb termek(ek) adatai:");
+            foreach (var legolcsobbTermek in legolcsobbak)
+            {
+                Console.WriteLine($"Kategória: {legolcsobbTermek.mufaj}, {legolcsobbTermek.cim}, Ár: {legolcsobbTermek.ar}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2024.03.11/1b/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Kategória = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cím = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ár = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Kategória,
+            this.Cím,
+            this.Ár});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(331, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Kategória
+            // 
+            this.Kategória.HeaderText = "Kategória";
+            this.Kategória.Name = "Kategória";
+            // 
+            // Cím
+            // 
+            this.Cím.HeaderText = "Cím";
+            this.Cím.Name = "Cím";
+            // 
+            // Ár
+            // 
+            this.Ár.HeaderText = "Ár";
+            this.Ár.Name = "Ár";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(302, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Válasszon egy kategóriát!";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Regény",
+            "Történelem",
+            "Sci-fi",
+            "Ifjúsági",
+            "Kulinária",
+            "Tudomány"});
+            this.comboBox1.Location = new System.Drawing.Point(305, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(528, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 199);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Könyvek";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kategória;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cím;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ár;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
